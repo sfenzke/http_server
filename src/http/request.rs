9de2f4bdb70397a8fc235d4a909bad28fd97e8b5
Stefan Fenzke <stafen.fenzke@gmx.de>
2022-04-14 18:46:41 +0200

    refactored some code and began implementation of request parser

--- src/http/request.rs
+++ src/http/request.rs
@@ -1,20 +1,10 @@
 mod error;
+mod method;
 
 use std::convert::TryFrom;
 use error::ParseError;
+use method::Method;
 use std::str;
-
-enum Method {
-    GET,
-    POST,
-    PUT,
-    HEAD,
-    DELETE,
-    CONNECT,
-    OPTIONS,
-    TRACE,
-    PATCH
-}
 
 pub struct Request {
     path: String,
@@ -29,9 +19,23 @@ impl TryFrom<&[u8]> for Request {
         let req_data = str::from_utf8(buffer)?;
         
         let mut req_line_iterator = req_data.lines();
-        let first_line = req_line_iterator.next().ok_or(ParseError::InvalidRequest)?;
-        println!("{}", first_line);
 
-        unimplemented!()
+        // get the first line from the request header and split in into segments
+        // it should look like: GET /?id=0 HTTP/1.1
+        let first_line = (req_line_iterator.next().
+                            ok_or(ParseError::InvalidRequest)?).
+                            split_ascii_whitespace().
+                            collect::<Vec<&str>>();
+        
+        // if there are not exactly 3 elements in the vector containing
+        // the elements of the first line, the request is invalid
+        match first_line.len() {
+            3 => Ok(Request {
+                    method: Method::try_from(first_line[0])?,
+                    path: first_line[1].to_string(),
+                    query_string: None
+            }),
+            _ => Err(ParseError::InvalidRequest)
+        }
     }   
 }
