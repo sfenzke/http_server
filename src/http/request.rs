b4c1af97378ac79810aabd95c22e1ed227e5a197
Stefan Fenzke <stefan.fenzke@noris.de>
2022-04-14 13:29:13 +0200

    bagen request implementaion

---
+++ src/http/request.rs
@@ -0,0 +1,17 @@
+mod error;
+
+use std::convert::{From, TryFrom};
+use error::ParseError;
+
+
+pub struct Request {
+
+}
+
+impl TryFrom<&[u8]> for Request {
+    type Error = ParseError;
+
+    fn try_from(buffer: &[u8]) -> Result<Self, Self::Error> {
+        !unimplemented!()
+    }   
+}
