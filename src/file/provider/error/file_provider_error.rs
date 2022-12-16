b758dde1be8fc751fd1b761ece2e7158cfbf26f2
Stefan Fenzke <stefan.fenzke@noris.de>
2022-12-16 12:05:02 +0100

    impleneted error types for FileProviderError

--- src/file/provider/error/file_provider_error.rs
+++ src/file/provider/error/file_provider_error.rs
@@ -1,14 +1,31 @@
 use std::{error::Error, fmt::Display};
 
 #[derive(Debug)]
+pub enum ErrorType {
+    FILE_NOT_FOUND,
+    READ_ERROR
+}
+
+impl Display for ErrorType {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+           Self::FILE_NOT_FOUND => { write!(f, "File bot found")},
+           Self::READ_ERROR => { write!(f, "Read error")}
+        }
+    }
+}
+
+#[derive(Debug)]
 pub struct FileProviderError {
-    message: String
+    message: String,
+    error_type: ErrorType
 }
 
 impl FileProviderError {
-    pub fn new(message: String) -> Self {
+    pub fn new(message: String, error_type: ErrorType ) -> Self {
         Self {
-            message
+            message,
+            error_type
         }
     }
 }
@@ -17,6 +34,6 @@ impl Error for FileProviderError {}
 
 impl Display for FileProviderError {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(f, "{}", self.message)
+        write!(f, "{}: {}", self.error_type, self.message)
     }
 }
