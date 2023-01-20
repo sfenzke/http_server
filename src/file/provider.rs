12a206b2f5f7d8bf2724ddc5127a5aa690b887a2
Stefan Fenzke <stefan.fenzke@noris.de>
2023-01-20 18:29:06 +0100

    fixxed an error while removing dummy_provider

--- src/file/provider.rs
+++ src/file/provider.rs
@@ -1,10 +1,8 @@
 mod error;
 
 pub mod file_provider;
-pub mod dummy_provider;
 pub mod simple_filesystem_provider;
 
 
 pub use file_provider::FileProvider;
-pub use dummy_provider::DummyProvider;
 pub use simple_filesystem_provider::SimpleFilesystemProvider;
